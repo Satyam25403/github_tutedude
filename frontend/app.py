1c8fc175848e4fbd4e9decfba02d75e8e99da0b2
Satyam250304 <228w1a05i5@vrsec.ac.in>
2025-09-04 16:13:55 +0530

    Add /submittodoitem POST route with MongoDB integration

---
+++ frontend/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+from dotenv import load_dotenv
+import os
+import pymongo
+
+
+app = Flask(__name__)
+
+load_dotenv()
+MONGO_URI=os.getenv('MONGO_URI')
+
+client=pymongo.MongoClient(MONGO_URI)
+db = client['todo_db']
+todos_collection = db['todos']
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo():
+    item_name = request.form['itemName']
+    item_desc = request.form['itemDescription']
+    todos_collection.insert_one({
+        'itemName': item_name,
+        'itemDescription': item_desc
+    })
+    return "Item successfully added!"
